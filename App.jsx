f44cd2c4c3ebd6b4309f3d64cd9e9d81c0015c0b
emanzahra5124-svg <emanzahra5124@gmail.com>
2026-06-19 18:45:14 +0500

    Add files via upload

---
+++ App.jsx
@@ -0,0 +1,14 @@
+import "./index.css";
+import "./App.css";
+import Quiz from "./components/quiz";
+
+function App() {
+  return (
+    <div className="app-container">
+      <h1>Quiz App</h1>
+      <Quiz /> 
+    </div>
+  );
+}
+
+export default App;
